e5ec8c87d7c59f450703abfec329e31248d0053a
Ivan Calderon <ivancalderon@keos.co>
2018-01-30 13:14:14 -0500

    Traducciones

---
+++ asset/js/functions.js
@@ -0,0 +1,12 @@
+var lang="en";
+import lang_obj from '../lang/lang';
+$(document).ready(function(){
+	setLanguage(lang);
+});
+
+function setLanguage(lang){
+	$("translate").each(function(index){
+		let text=$(this).data("text");
+		$(this).html(objLang[text][lang]);
+	});
+}
---
+++ asset/lang/lang.js
@@ -0,0 +1,5 @@
+var lang_obj={
+	home:{es:"Inicio",en:"Home"}
+}
+
+export const lang_obj = lang_obj;
