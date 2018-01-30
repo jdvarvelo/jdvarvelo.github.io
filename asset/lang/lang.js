c8925a0a1f8677eaa40c54c6a294f4dedd639878
Ivan Calderon <ivancalderon@keos.co>
2018-01-30 14:46:31 -0500

    Se agregó el click al boton de cambiar el lenguaje, ademas se soluciono el bug a la hora de importar el lang.js

--- asset/lang/lang.js
+++ asset/lang/lang.js
@@ -1,12 +1,12 @@
 var lang_obj={
 	home:{es:"Inicio",en:"Home"},
-	styles:{es:"Estilos",en"Styles"},
-	cv:{es:"Curriculum",en"CV"},
-	projects:{es:"Proyectos",en"Projects"},
-	codes:{es:"Códigos",en"Codes"},
-	news:{es:"Noticias",en"News"},
+	styles:{es:"Estilos",en:"Styles"},
+	cv:{es:"Curriculum",en:"CV"},
+	projects:{es:"Proyectos",en:"Projects"},
+	codes:{es:"Códigos",en:"Codes"},
+	news:{es:"Noticias",en:"News"},
 	firstLanguage:{es:"Español",en:"English"},
-	secondLanguage:{es:"Ingles",en:"Spanish"},	
+	secondLanguage:{es:"Ingles",en:"Spanish"}
 }
 
 export default lang_obj;
