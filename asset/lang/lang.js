a61bdb62eb0eb4c73e586f6249b4469184677085
Ivan Calderon <ivancalderon@keos.co>
2018-01-30 17:39:50 -0500

    Se hizo lo siguiente 1.) Se automatizó la traducción 2.) Se colocó la session de bienvenida 3.) Se coloćo la session de redes sociales

--- asset/lang/lang.js
+++ asset/lang/lang.js
@@ -6,7 +6,13 @@ var lang_obj={
 	codes:{es:"Códigos",en:"Codes"},
 	news:{es:"Noticias",en:"News"},
 	firstLanguage:{es:"Español",en:"English"},
-	secondLanguage:{es:"Ingles",en:"Spanish"}
+	secondLanguage:{es:"Ingles",en:"Spanish"},
+	welcome:{es:" les da la bienvenida",en:" gives you welcome"},
+	thanks:{
+		es:"Muchas gracias por visitar esté sitio, el cual tiene la finalidad de compartir noticias, códigos e información en general relacionado con el desarrollo de software",
+		en:"Thank you very much for visiting this site, which has the purpose of sharing news, codes and information in general related to software development",
+	},
+	socialNetworks:{es:'Por favor visita mis redes sociales',en:'Please visit my social networks'}
 }
 
 export default lang_obj;
