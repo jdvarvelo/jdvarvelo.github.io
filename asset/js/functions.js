919be430045dccff4569c294f77e5af05485245e
Ivan Calderon <ivancalderon@keos.co>
2018-01-30 14:42:28 -0500

    Se agregó el click al boton de cambiar el lenguaje, ademas se soluciono el bug a la hora de importar el lang.js

--- asset/js/functions.js
+++ asset/js/functions.js
@@ -2,6 +2,12 @@ var lang="en";
 import lang_obj from '../lang/lang.js';
 $(document).ready(function(){
 	setLanguage(lang);
+
+	$(document).on("click","a#changeLanguage",function(e){
+		e.preventDefault();
+		lang=lang=="en"?"es":"en";
+		setLanguage(lang);
+	});
 });
 
 function setLanguage(lang){
