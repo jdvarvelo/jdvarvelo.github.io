a74dadf62e2d170d359449ca706e72ace02336f5
ivan calderon <ivandario2010@gmail.com>
2018-02-03 05:20:39 -0500

    Estructura del home

--- asset/lang/lang.js
+++ asset/lang/lang.js
@@ -1,16 +1,16 @@
 var lang_obj={
 	home:{es:"Inicio",en:"Home"},
-	styles:{es:"Estilos",en:"Styles"},
-	cv:{es:"Curriculum",en:"CV"},
-	projects:{es:"Proyectos",en:"Projects"},
-	codes:{es:"Códigos",en:"Codes"},
+	personalData:{es:"Datos Personales",en:"Personal Information"},
+	appliedStudies:{es:"Estudios Realizados",en:"Applied Studies"},
+	skills:{es:"Habilidades",en:"Skills"},
+	contact:{es:"Contacto",en:"Contact"},
 	news:{es:"Noticias",en:"News"},
 	firstLanguage:{es:"Español",en:"English"},
 	secondLanguage:{es:"Ingles",en:"Spanish"},
 	welcome:{es:" les da la bienvenida",en:" gives you welcome"},
 	thanks:{
-		es:"Muchas gracias por visitar esté sitio, el cual tiene la finalidad de compartir noticias, códigos e información en general relacionado con el desarrollo de software",
-		en:"Thank you very much for visiting this site, which has the purpose of sharing news, codes and information in general related to software development",
+		es:"Muchas gracias por interesarte en mi perfil profesional ",
+		en:"Thank you very much for taking an interest in my professional profile",
 	},
 	socialNetworks:{es:'Por favor visita mis redes sociales',en:'Please visit my social networks'},
 	contactus:{es:'Contactanos',en:'Contact us'},
@@ -25,7 +25,13 @@ var lang_obj={
 	otherSubject:{es:'Otro asunto',en:'Other subject'},
 	message:{es:'Mensaje',en:'Message'},
 	send:{es:'Enviar',en:'Send'},
-	cancel:{es:'Cancelar',en:'Cancel'}
+	cancel:{es:'Cancelar',en:'Cancel'},
+	data:{es:'Dato',en:'Data'},
+	value:{es:'Valor',en:'Value'},
+	surname:{es:'Apellido',en:'Surname'},
+	address:{es:'Dirección',en:'Address'},
+	phone:{es:'Teléfono',en:'Phone'},
+	dob:{es:'Fecha de nacimiento',en:'Date of Birth'}
 }
 
 export default lang_obj;
